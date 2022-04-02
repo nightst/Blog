ee4a06a8dc232f9845cd5e13b9821f76d22b42ab
nightst <nightst117170@gmail.com>
2022-04-02 16:02:11 +0800

    feat: add routing module and home page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+
+
+const route: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(route)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
