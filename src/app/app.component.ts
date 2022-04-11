fd11cde700be570276b104e3daeae4027fa5c594
nightst <nightst117170@gmail.com>
2022-04-11 17:31:48 +0800

    feat(home): add post card to home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor() {
+    this.test();
+    console.log(this.lists);
+  }
   logo = '云峰的博客';
   home = '主页';
   list = '文章';
   about = '关于';
+  lists: {title: string}[] = []
+  test () {
+    for (let i = 0; i < 50; i++) {
+      let temp = {
+        title: `标题${i}`
+      }
+      this.lists.push(temp);
+    }
+  }
+
+  ngOnInit(): void {
+  }
 }
