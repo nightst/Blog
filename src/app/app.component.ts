9b1551bd223de1f15215835821b1a418fe5b19d0
nightst <nightst117170@gmail.com>
2022-04-12 17:28:52 +0800

    feat(home): finish home page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   constructor() {
-    this.test();
-    console.log(this.lists);
   }
   logo = '云峰的博客';
   home = '主页';
   list = '文章';
   about = '关于';
-  lists: {title: string}[] = []
-  test () {
-    for (let i = 0; i < 50; i++) {
-      let temp = {
-        title: `标题${i}`
-      }
-      this.lists.push(temp);
-    }
-  }
-
-  ngOnInit(): void {
-  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,8 +9,37 @@ export class HomeComponent implements OnInit {
 
   constructor() {
   }
-
-  ngOnInit(): void {
+  lists: post[] = []
+  test () {
+    for (let i = 0; i < 50; i++) {
+      let temp: post = {
+        id: i,
+        title: `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa${i}`,
+        description: `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa${i}`,
+        totalNum: 100,
+        tags: ['日常', '技术'],
+        date: new Date()
+      };
+      // temp.title = `标题${i}`;
+      // temp.description = `描述${i}`;
+      // temp.totalNum = 100;
+      // temp.tag = ['日常', '技术'];
+      // temp.date = new Date();
+      this.lists.push(temp);
+    }
   }
 
+  ngOnInit(): void {
+    this.test();
+  }
+
+}
+
+export interface post {
+  id: number,
+  title: string,
+  description: string,
+  totalNum: number,
+  tags: string[],
+  date: Date
 }
