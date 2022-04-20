78a6780c78f86d9551954fbbe30aeeae4caded8d
nightst <529379231@qq.com>
2022-04-20 23:04:20 +0800

    feat(list): add list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from "@angular/router";
-import { AboutComponent } from "./about/about.component";
-import { HomeComponent } from "./home/home.component";
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {AboutComponent} from "./about/about.component";
+import {HomeComponent} from "./home/home.component";
+import {PostListComponent} from "./post-list/post-list.component";
 
 
 const route: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'list', component: PostListComponent},
 ]
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent],
+  declarations: [HomeComponent, AboutComponent, PostListComponent],
   imports: [
     CommonModule,
     RouterModule.forRoot(route)
