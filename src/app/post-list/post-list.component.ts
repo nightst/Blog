226e949a2629c0aad4b7dd51a7ddb40485fc0088
nightst <529379231@qq.com>
2022-04-21 23:58:46 +0800

    feat(list): add conotent to list page

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-post-list',
@@ -7,10 +8,50 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  constructor() {
+  tag: string = 'date';
+  list: post[] = [];
+  yearList: string[] = ['2022', '2021']
+
+  constructor(
+    private route: ActivatedRoute
+  ) {
+  }
+
+  test() {
+    for (let i = 0; i < 50; i++) {
+      let temp: post = {
+        id: i,
+        title: `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa${i}`,
+        description: `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa${i}`,
+        totalNum: 100,
+        tags: ['日常', '技术'],
+        date: new Date()
+      };
+      // temp.title = `标题${i}`;
+      // temp.description = `描述${i}`;
+      // temp.totalNum = 100;
+      // temp.tag = ['日常', '技术'];
+      // temp.date = new Date();
+      this.list.push(temp);
+    }
   }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(
+      next => {
+        this.tag = next['tag'];
+      }
+    )
+    this.test();
   }
 
+}
+
+export interface post {
+  id: number,
+  title: string,
+  description: string,
+  totalNum: number,
+  tags: string[],
+  date: Date
 }
