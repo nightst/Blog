b55ac4e81caaace80ce00a6ff2d0e58fce71f7ff
nightst <529379231@qq.com>
2022-05-07 17:44:23 +0800

    feat(home): now the home page can run correctly

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from "../app.service";
 
 @Component({
   selector: 'app-home',
@@ -7,30 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    private appService: AppService
+  ) {
   }
+
   lists: post[] = []
-  test () {
-    for (let i = 0; i < 50; i++) {
-      let temp: post = {
-        id: i,
-        title: `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa${i}`,
-        description: `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa${i}`,
-        totalNum: 100,
-        tags: ['日常', '技术'],
-        date: new Date()
-      };
-      // temp.title = `标题${i}`;
-      // temp.description = `描述${i}`;
-      // temp.totalNum = 100;
-      // temp.tag = ['日常', '技术'];
-      // temp.date = new Date();
-      this.lists.push(temp);
-    }
+
+  getAllPosts(): void {
+    this.appService.getAssets('posts').subscribe({
+      next: (next) => {
+        if (next && next.postList) {
+          this.lists = next.postList;
+        }
+      },
+      error: () => {
+        window.alert('Get posts from assets fails');
+      }
+    })
   }
 
   ngOnInit(): void {
-    this.test();
+    this.getAllPosts();
   }
 
 }
@@ -41,5 +40,5 @@ export interface post {
   description: string,
   totalNum: number,
   tags: string[],
-  date: Date
+  date: string
 }
