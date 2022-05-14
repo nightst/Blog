aa3d8a0f501399c00dd2fd36b5823da75d5cfa94
nightst <529379231@qq.com>
2022-05-14 21:36:07 +0800

    feat(posts): add post4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { PostListComponent } from "./post-list/post-list.component";
 import { Post1Component } from "./posts/post1/post1.component";
 import { Post2Component } from "./posts/post2/post2.component";
 import { Post3Component } from "./posts/post3/post3.component";
+import { Post4Component } from "./posts/post4/post4.component";
 
 
 const route: Routes = [
@@ -19,11 +20,12 @@ const route: Routes = [
   {path: 'list/1', component: Post1Component},
   {path: 'list/2', component: Post2Component},
   {path: 'list/3', component: Post3Component},
+  {path: 'list/4', component: Post4Component},
   {path: '**', component: PageNotFoundComponent},
 ]
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent, PostListComponent, PageNotFoundComponent, Post1Component, Post2Component, Post3Component],
+  declarations: [HomeComponent, AboutComponent, PostListComponent, PageNotFoundComponent, Post1Component, Post2Component, Post3Component, Post4Component],
   imports: [
     CommonModule,
     RouterModule.forRoot(route),
