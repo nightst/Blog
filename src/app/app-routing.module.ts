e0b8175dfdae54f17650c9b159875951a932571a
nightst <529379231@qq.com>
2022-05-18 13:18:01 +0800

    feat(posts): add post11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 import { PostListComponent } from "./post-list/post-list.component";
 import { Post1Component } from "./posts/post1/post1.component";
 import { Post10Component } from "./posts/post10/post10.component";
+import { Post11Component } from "./posts/post11/post11.component";
 import { Post2Component } from "./posts/post2/post2.component";
 import { Post3Component } from "./posts/post3/post3.component";
 import { Post4Component } from "./posts/post4/post4.component";
@@ -33,11 +34,12 @@ const route: Routes = [
   {path: 'list/8', component: Post8Component},
   {path: 'list/9', component: Post9Component},
   {path: 'list/10', component: Post10Component},
+  {path: 'list/11', component: Post11Component},
   {path: '**', component: PageNotFoundComponent},
 ]
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent, PostListComponent, PageNotFoundComponent, Post1Component, Post2Component, Post3Component, Post4Component, Post6Component, Post7Component, Post8Component, Post9Component, Post10Component],
+  declarations: [HomeComponent, AboutComponent, PostListComponent, PageNotFoundComponent, Post1Component, Post2Component, Post3Component, Post4Component, Post6Component, Post7Component, Post8Component, Post9Component, Post10Component, Post11Component],
   imports: [
     CommonModule,
     RouterModule.forRoot(route),
