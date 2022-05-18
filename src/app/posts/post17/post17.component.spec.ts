89e83632337ccd9ca8e181e54bf4419d74fe3ac0
nightst <529379231@qq.com>
2022-05-18 14:46:53 +0800

    feat(posts): add post17

---
+++ src/app/posts/post17/post17.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Post17Component } from './post17.component';
+
+describe('Post17Component', () => {
+  let component: Post17Component;
+  let fixture: ComponentFixture<Post17Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [Post17Component]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Post17Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
