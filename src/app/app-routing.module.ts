0905170cdbb192fcc753a42916e0bc19384b37dd
nightst <529379231@qq.com>
2022-12-24 14:41:50 +0800

    feat(posts): add post20

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ import { Post17Component } from "./posts/post17/post17.component";
 import { Post18Component } from "./posts/post18/post18.component";
 import { Post19Component } from "./posts/post19/post19.component";
 import { Post2Component } from "./posts/post2/post2.component";
+import { Post20Component } from "./posts/post20/post20.component";
 import { Post3Component } from "./posts/post3/post3.component";
 import { Post4Component } from "./posts/post4/post4.component";
 import { Post5Component } from "./posts/post5/post5.component";
@@ -51,6 +52,7 @@ const route: Routes = [
   {path: 'list/17', component: Post17Component},
   {path: 'list/18', component: Post18Component},
   {path: 'list/19', component: Post19Component},
+  {path: 'list/20', component: Post20Component},
   {path: '**', component: PageNotFoundComponent},
 ]
 
