9087dbfe39bcdaf6fb83e936b737160096da1d2c
nightst <529379231@qq.com>
2022-12-24 14:48:20 +0800

    feat(posts): add post21

---
+++ src/app/posts/post21/post21.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post21',
+  templateUrl: './post21.component.html',
+  styleUrls: ['./post21.component.css']
+})
+export class Post21Component implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
